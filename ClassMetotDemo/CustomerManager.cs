3ad8e39ea25b742c626c84e3c66ff37fca6edc84
Mine Aktaş <mineeakts@gmail.com>
2021-02-28 02:34:12 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine(customer.Name + " Added!");
+
+        }
+        public void Delete(Customer customer)
+        {
+            customers.Remove(customer);
+            Console.WriteLine(customer.Name + " Deleted!");
+        }
+        public void List()
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customer Id : " + customer.Id + " Customer Name : " + customer.Name);
+                Console.WriteLine("***********");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Mine Aktaş";
+            customer1.Job = "Software Developer";
+            customer1.Age = 22;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Ahmet Aktaş";
+            customer2.Job = "Dentist";
+            customer2.Age = 20;
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.List();
+
+            customerManager.Delete(customer2);
+            customerManager.List();
+        }
+    }
+}
